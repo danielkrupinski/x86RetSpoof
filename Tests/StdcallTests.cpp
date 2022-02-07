69850acd2c482c1280cb6756fb88c5e862dd681e
Daniel Krupiński <danielkrupinski@outlook.com>
2022-02-07 22:19:35 +0100

    Implement a unit test for x86RetSpoof::invokeStdcall() checking the return address

---
+++ Tests/StdcallTests.cpp
@@ -0,0 +1,19 @@
+#include <array>
+#include <cstdint>
+#include <intrin.h>
+
+#include <gtest/gtest.h>
+
+#include "x86RetSpoof.h"
+
+#pragma section(".text")
+__declspec(allocate(".text")) constexpr std::array<std::uint8_t, 2> gadget{ 0xFF, 0x23 }; // jmp dword ptr[ebx]
+
+static void* __stdcall getReturnAddressOfMyself()
+{
+    return _ReturnAddress();
+}
+
+TEST(InvokeStdcallTest, ReturnAddressOfTheInvokedFunctionIsTheAddressOfTheGadget) {
+    EXPECT_EQ(x86RetSpoof::invokeStdcall<void*>(std::uintptr_t(&getReturnAddressOfMyself), std::uintptr_t(gadget.data())), gadget.data());
+}
